e4b2bd528642b5ae590968cb32dd0fef17391987
Kirkkm <matthewkk7@gmail.com>
2025-09-16 11:30:22 -0400

    Finishing up the functionality for Ability and adding its tests

--- src/Ability.java
+++ src/Ability.java
@@ -1,17 +1,37 @@
 public class Ability {
-    String name;
-    String description;
-    String type;
+    private final String name;
+    private final String description;
+    private final String type;
+    private final Stats statsEffect;
 
-    public Ability(String name, String description, String type) {
+    public Ability(String name, String description, String type, Stats statsEffect) {
         this.name = name;
         this.description = description;
         this.type = type;
+        this.statsEffect = statsEffect;
     }
-    // public int effect(){};
+
+    public String getName() {
+        return this.name;
+    }
+    public String getDescription() {
+        return this.description;
+    }
+    public String getType() {
+        return this.type;
+    }
+
+    public Stats useAbility(Stats stats) {
+        stats.retoreHealth(this.statsEffect.getHealth());
+        stats.updateBaseAttack(this.statsEffect.getBaseAttack());
+        stats.updateDefensePoints(this.statsEffect.getDefensePoints());
+
+        return stats;
+    };
 
     @Override
     public String toString() {
-        return "Name: " + this.name + "\nDescription: " + this.description + "\nType: " + this.type;
+        String statusEffect = "Status Effect\n=========\nHealth: "+this.statsEffect.getHealth()+"\nBase Attack: "+ this.statsEffect.getBaseAttack()+"\nDefense: "+this.statsEffect.getDefensePoints();
+        return "Name: " + this.name + "\nDescription: " + this.description + "\nType: " + this.type+"\n\n" + statusEffect;
     }
 }
---
+++ tests/AbilityTest.java
@@ -0,0 +1,53 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AbilityTest {
+
+    private Ability brace;
+    private Stats statsTest;
+
+    @BeforeEach
+    void setUp() {
+        brace = new Ability("Brace", "Raise your defences to prepare for an attack", "Universal", new Stats(0, 0, 500));
+        statsTest = new Stats(1000, 1000, 500);
+    }
+
+    @Test
+    void getName() {
+        assertEquals("Brace", brace.getName());
+    }
+    @Test
+    void getDescription() {
+        assertEquals("Raise your defences to prepare for an attack", brace.getDescription());
+    }
+
+    @Test
+    void getType() {
+        assertEquals("Universal", brace.getType());
+    }
+
+    @Test
+    void useAbility() {
+        Stats updatedStats = brace.useAbility(statsTest);
+        assertEquals(1000, updatedStats.getHealth());
+        assertEquals(1000, updatedStats.getBaseAttack());
+        assertEquals(1000, updatedStats.getDefensePoints());
+    }
+
+    @Test
+    void testToString() {
+        String expect = """
+                Name: Brace
+                Description: Raise your defences to prepare for an attack
+                Type: Universal
+                
+                Status Effect
+                =========
+                Health: 0
+                Base Attack: 0
+                Defense: 500""";
+        assertEquals(expect, brace.toString());
+    }
+}
