77abd79ef8918dea52b6e4c618b03773fd7ae804
Kirkkm <matthewkk7@gmail.com>
2025-09-23 14:46:26 -0400

    Adding logic for Aliens regen & mutate methods

--- src/Alien.java
+++ src/Alien.java
@@ -1,6 +1,9 @@
-public class Alien extends Entity {
-    public Alien(String name, String type){
+import java.util.Random;
 
+public class Alien extends Entity {
+    private int regenAmount;
+
+    public Alien(String name, String type){
         int health;
         int baseAttackPoint;
         int defensePoints;
@@ -9,22 +12,39 @@ public class Alien extends Entity {
             health = 1000;
             baseAttackPoint = 10;
             defensePoints = 1200;
+            this.regenAmount = 5;
         }
         else if (type.equals("Attacker")) {
             health = 300;
             baseAttackPoint = 1200;
             defensePoints = 200;
+            this.regenAmount = 15;
         }
         else {
             System.out.println("No valid type provided, using base type");
             health = 500;
             baseAttackPoint = 500;
             defensePoints = 500;
+            this.regenAmount = 10;
         }
 
         super(name, health, health, baseAttackPoint, defensePoints);
     }
 
-    public void mutate(){}
-    public void regen(){}
+    public void mutate() {
+        Random randomGenerator = new Random();
+
+        int randomHealthStat = randomGenerator.nextInt(100);
+        int randomAttackStat = randomGenerator.nextInt(100);
+        int randomDefenseStat = randomGenerator.nextInt(100);
+
+        this.getStats().increaseHealthPoints(randomHealthStat);
+        this.getStats().retoreHealth(randomHealthStat);
+        this.getStats().updateBaseAttack(randomAttackStat);
+        this.getStats().updateDefensePoints(randomDefenseStat);
+    }
+
+    public void regen() {
+        this.getStats().retoreHealth(this.regenAmount);
+    }
 }
